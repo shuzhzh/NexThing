2acd12550e2b385630bb0043d55cc169e4c9a8f8
Tim Shu <shuzhzh@gmail.com>
2023-10-18 08:21:12 +0000

    V0.4设计一个网页可自适应长度的输入框，提示语显示：我准备做。后面有一个紫色的提交按钮，输入的文字提交之后显示在输入框的顶部居中显示。每整点清除掉显示的输入内容，每整点内可以重复提交内容，每次提交覆盖之前已提交显示的内容。拆开html，css和js文件

--- js/HomPage_Script.js
+++ js/HomPage_Script.js
@@ -1,111 +1,19 @@
-document.getElementById('baidu_search_button').addEventListener('click', function() {  
-  // 获取搜索关键词  
-  var keyword = document.getElementById('baidu_search_input').value;  
-  
-  // 构建百度搜索的URL  
-  var baiduUrl = 'https://www.baidu.com/s?wd=' + encodeURIComponent(keyword);  
-  
-  // 跳转到百度搜索结果页  
-  window.location.href = baiduUrl;  
-});
+document.addEventListener('DOMContentLoaded', function() {
+    const userInput = document.getElementById('userInput');
+    const submitButton = document.getElementById('submitButton');
+    const outputText = document.getElementById('outputText');
 
-// 获取按钮元素
-const buttons = document.querySelectorAll('button');
+    submitButton.addEventListener('click', function() {
+        const inputValue = userInput.value.trim();
+        if (inputValue !== '') {
+            outputText.innerHTML = inputValue;
+        }
+    });
 
-// 存储点击次数的对象
-let clicks = {};
-
-// 监听按钮点击事件
-buttons.forEach(button => {
-  button.addEventListener('click', function() {
-    // 获取当前时间（小时）
-    const hour = new Date().getHours();
-
-    // 如果点击次数对象中没有该小时的数据，则创建一个空数组
-    if (!clicks[hour]) {
-      clicks[hour] = [];
+    function clearOutputText() {
+        outputText.innerHTML = '';
     }
 
-    // 将当前按钮的点击次数添加到该小时的数组中
-    clicks[hour].push(this.id);
-
-    // 计算并显示按钮点击次数占比
-    const result = calculatePercentage(clicks);
-    document.getElementById('result').innerHTML = result;
-  });
+    // 每整点清除显示的内容
+    setInterval(clearOutputText, 3600000);
 });
-
-// 计算按钮点击次数占比的函数
-function calculatePercentage(clicks) {
-  // 计算每个按钮在当前小时的点击次数
-  const button1Count = clicks['0'].filter(id => id === 'button1').length;
-  const button2Count = clicks['0'].filter(id => id === 'button2').length;
-  const button3Count = clicks['0'].filter(id => id === 'button3').length;
-
-  // 计算每个按钮点击次数占比
-  const button1Percentage = button1
-
-  // 获取输入框和按钮元素
-const labelInput = document.getElementById('labelInput');
-const countButton = document.getElementById('countButton');
-const result = document.getElementById('result');
-
-// 存储点击次数的对象
-let clicks = {};
-
-// 监听按钮点击事件
-countButton.addEventListener('click', function() {
-  // 获取输入框中的标签文案
-  const label = labelInput.value;
-
-  // 如果点击次数对象中没有该标签文案的数据，则创建一个空数组
-  if (!clicks[label]) {
-    clicks[label] = [];
-  }
-
-  // 将当前标签的点击次数添加到该标签文案的数组中
-  clicks[label].push(label);
-
-  // 计算并显示标签点击次数占比
-  const percentage = calculatePercentage(clicks);
-  result.innerHTML = percentage;
-});
-
-// 计算标签点击次数占比的函数
-function calculatePercentage(clicks) {
-  // 计算每个标签文案在当前小时的点击次数
-  const labelCount = Object.values(clicks).reduce((a, b) => a + b.length, 0);
-
-  // 计算标签点击次数占比
-  const percentage = labelCount / Object.keys(clicks).length;
-
-  return percentage.toFixed(2);
-}
-
-document.addEventListener('DOMContentLoaded', function () {  
-    const icons = document.querySelectorAll('.icon');  
-    let clicks = JSON.parse(localStorage.getItem('clicks')) || { icon1: 0, icon2: 0, icon3: 0 };  
-  
-    icons.forEach((icon, index) => {  
-        icon.addEventListener('click', function () {  
-            clicks[`icon${index + 1}`]++;  
-            localStorage.setItem('clicks', JSON.stringify(clicks));  
-            displayResult();  
-        });  
-    });  
-    displayResult();  
-});
-
-function displayResult() {  
-    const clicks = JSON.parse(localStorage.getItem('clicks'));  
-    const totalClicks = Object.values(clicks).reduce((a, b) => a + b, 0);  
-    const resultElement = document.getElementById('result');  
-    let result = '';  
-  
-    for (const key in clicks) {  
-        const percentage = ((clicks[key] / totalClicks) * 100).toFixed(2);  
-        result += `<p>${key}: ${clicks[key]} (${percentage}%)</p>`;  
-    }  
-  
-    resultElement.innerHTML = result;  
-}
